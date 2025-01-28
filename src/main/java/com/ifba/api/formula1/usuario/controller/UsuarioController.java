13784cf0462fcd7225276533476cc53af3ef65cc
DevMartins <h.martins.silva2014@gmail.com>
2025-01-28 00:07:43 -0300

    :ok_hand: Reajustando Controller para aceitar todas as requisições

--- src/main/java/com/ifba/api/formula1/usuario/controller/UsuarioController.java
+++ src/main/java/com/ifba/api/formula1/usuario/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.ifba.api.formula1.usuario.controller;
 import com.ifba.api.formula1.usuario.entity.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +15,22 @@ public class UsuarioController {
     @GetMapping(produces = "application/json")
     public  ResponseEntity<?> findByAll(){
         return ResponseEntity.status(HttpStatus.OK).body("Lista de usuarios");
+
     }
-    @PostMapping(path = "/save", consumes = "application/json")
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> save(@RequestBody Usuario usuario){
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+
     }
 
-    @PutMapping(path = "/update", consumes = "application/json")
-    public  ResponseEntity<?> update(){
-        return ResponseEntity.status(HttpStatus.OK).body("Atualizando");
+    @PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public  ResponseEntity<Void> update(@RequestBody Usuario usuario){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
-    @DeleteMapping(path = "{id}", consumes = "application/json")
-    public  ResponseEntity<?> delete(){
+
+    @DeleteMapping(path = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public  ResponseEntity<?> delete(@PathVariable("id") long id){
         return ResponseEntity.status(HttpStatus.OK).body("Delete");
     }
 }
