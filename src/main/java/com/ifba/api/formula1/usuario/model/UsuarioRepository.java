2dd93722286591e5dbcf7bec8e955bee01faf669
DevMartins <h.martins.silva2014@gmail.com>
2025-01-31 01:33:48 -0300

    fead:  Adicionando DTO em Busca e Salvar

--- src/main/java/com/ifba/api/formula1/usuario/model/UsuarioRepository.java
+++ src/main/java/com/ifba/api/formula1/usuario/model/UsuarioRepository.java
@@ -1,13 +1,4 @@
 package com.ifba.api.formula1.usuario.model;
 
-import com.ifba.api.formula1.usuario.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
+public interface UsuarioRepository {
 }
